e761396d93d0714e58bffbc0a9c84be2a70a21ca
Jeff Martin <jeffrey.j.martin2@gmail.com>
2021-11-26 19:38:48 -0500

    WIP colorized bars, no legend

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Native
 import os
 import sqlite3 as sql
+import textwrap
 
 # Third party
 from codetiming import Timer
@@ -13,6 +14,7 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import plotly.graph_objects as go
 
 # Custom
 import lib.database_helper as db
@@ -24,7 +26,16 @@ server = app.server
 
 db_path = os.path.abspath('data/books.db')
 with db.connect(db_path) as con:
-    df = pd.read_sql('SELECT * FROM books_kaggle', con)
+    sql = """
+        SELECT ta.isbn13, ta.author, ta.title, c.cat_descrip, bk.ratings_count, bk.average_rating
+        FROM titles__authors ta, titles__cat_codes tcc, books_kaggle bk, categories c 
+        WHERE ta.isbn13 = tcc.isbn13
+            AND ta.isbn13 = bk.isbn13
+            AND c.cat_code = tcc.cat_code 
+            and bk.ratings_count > 100
+        ORDER BY bk.average_rating DESC;
+    """
+    df = pd.read_sql(sql, con)
 
 app.layout = html.Div(className='base-page', children=[
 
@@ -38,10 +49,80 @@ app.layout = html.Div(className='base-page', children=[
 
     # Main
     html.Div([
-        'placeholder'
+        # Text
+        'Choose an author',
+
+        # Dropdown filter
+        dcc.Dropdown(
+            id='dropdown',
+            options=[
+                {'label': label, 'value': label}
+                for label
+                in df['author'].sort_values().unique()
+            ],
+            value=['David Foster Wallace', 'J. K. Rowling', 'Stephen King'],
+            multi=True,
+        ),
+
+        # Graph
+        dcc.Graph(id='graph'),
     ])
 
 ])
+
+# @app.callback(
+#     Output('graph', 'figure'),
+#     Input('dropdown', 'value')
+# )
+# def update_graph(authors: list):
+#     filtered_df = df.loc[df.author.isin(authors)]
+#     filtered_df = filtered_df.sort_values(by='average_rating')
+#     filtered_df = filtered_df.drop_duplicates(subset=['title'])
+#     filtered_df = filtered_df.drop_duplicates(subset=['isbn13'])
+#     # filtered_df['title'] = filtered_df['title'].transform(lambda x: '\n'.join(textwrap.wrap(x, width=10)))
+
+#     fig = px.bar(filtered_df,
+#                   x='average_rating',
+#                   y='title',
+#                   color='author',
+#                   orientation='h',
+#                   text='average_rating',
+#                   height=len(filtered_df) * 50,
+#                   )
+#     fig.update_layout(barmode='stack', xaxis={'categoryorder': 'total descending'})
+#     return fig
+
+@app.callback(
+    Output('graph', 'figure'),
+    Input('dropdown', 'value')
+)
+def update_graph(authors: list):
+    filtered_df = df.loc[df.author.isin(authors)]
+    filtered_df = filtered_df.sort_values(by='average_rating')
+    filtered_df = filtered_df.drop_duplicates(subset=['title'])
+    filtered_df = filtered_df.drop_duplicates(subset=['isbn13'])
+
+    # Set author colors
+    authors_unique = filtered_df['author'].unique()
+    authors_num = len(authors_unique)
+    author_colors_all = px.colors.qualitative.Plotly[:authors_num]
+    author_colors_repl = {author: color for author, color in zip(authors_unique, author_colors_all)}
+    filtered_df['author_color'] = filtered_df['author'].replace(author_colors_repl)
+
+    fig = go.Figure()
+    
+    fig.add_trace(go.Bar(
+        x=filtered_df['average_rating'],
+        y=filtered_df['title'],
+        orientation='h',
+        marker_color=filtered_df['author_color'],
+        text=filtered_df['average_rating'],
+    ))
+    fig.update_layout(
+        height=len(filtered_df) * 40,
+        title='Book Titles with Average Goodreads Ratings',
+    )
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- lib/database_helper.py
+++ lib/database_helper.py
@@ -1,1 +1,10 @@
-from sqlalchemy import create_engine
+from contextlib import contextmanager
+import os
+import sqlite3 as sql
+
+@contextmanager
+def connect(path: os.PathLike):
+    con = sql.connect(path)
+    yield con
+    con.commit()
+    con.close()
